b1b9355770a15cd9b076751f245103dfa93b897d
Eric Jameson <e-jameson@bethel.edu>
2015-04-09 14:36:43 -0500

    add some commas

--- descriptions.py
+++ descriptions.py
@@ -2,14 +2,14 @@ __author__ = 'ejc84332'
 
 
 delivery_descriptions = {
-    'HL': 'You’ll complete the majority of your classes and assignments online, with periodic face to face meetings at one of our convenient locations.'
-    'H': 'You’ll complete the majority of your classes and assignments online, with periodic face to face meetings at one of our convenient locations.'
-    'HH': 'You’ll complete the majority of your classes and assignments online, with periodic face to face meetings at one of our convenient locations.'
-    'OI': 'You’ll complete classes and assignments online, allowing you to interact with faculty, have discussions, and encourage your classmates all from a distance.'
-    'F2': 'You’ll meet for classes at one of our convenient locations and move through your program with the same cohort of supportive colleagues.' 
-    'O': 'You’ll complete classes and assignments online, allowing you to interact with faculty, have discussions, and encourage your classmates all from a distance.'
-    'OO': 'You’ll complete classes and assignments online, allowing you to interact with faculty, have discussions, and encourage your classmates all from a distance.'
-    'F': 'You’ll meet for classes at one of our convenient locations and move through your program with the same cohort of supportive colleagues.'
+    'HL': 'You’ll complete the majority of your classes and assignments online, with periodic face to face meetings at one of our convenient locations.',
+    'H': 'You’ll complete the majority of your classes and assignments online, with periodic face to face meetings at one of our convenient locations.',
+    'HH': 'You’ll complete the majority of your classes and assignments online, with periodic face to face meetings at one of our convenient locations.',
+    'OI': 'You’ll complete classes and assignments online, allowing you to interact with faculty, have discussions, and encourage your classmates all from a distance.',
+    'F2': 'You’ll meet for classes at one of our convenient locations and move through your program with the same cohort of supportive colleagues.' ,
+    'O': 'You’ll complete classes and assignments online, allowing you to interact with faculty, have discussions, and encourage your classmates all from a distance.',
+    'OO': 'You’ll complete classes and assignments online, allowing you to interact with faculty, have discussions, and encourage your classmates all from a distance.',
+    'F': 'You’ll meet for classes at one of our convenient locations and move through your program with the same cohort of supportive colleagues.',
 }
 
 locations = {
