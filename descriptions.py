1146847c8aae4b195e86efc048161c1685de87ee
ErikGruber <erik-gruber@bethel.edu>
2015-04-09 14:27:07 -0500

    Update descriptions.py

--- descriptions.py
+++ descriptions.py
@@ -2,9 +2,9 @@ __author__ = 'ejc84332'
 
 
 delivery_descriptions = {
-    'HL': 'Blended',
-    'H': '',
-    'HH': 'Blended',
+    'HL': 'Blended', 'You’ll complete the majority of your classes and assignments online, with periodic face to face meetings at one of our convenient locations.'
+    'H': '', 'You’ll complete the majority of your classes and assignments online, with periodic face to face meetings at one of our convenient locations.'
+    'HH': 'Blended', 'You’ll complete the majority of your classes and assignments online, with periodic face to face meetings at one of our convenient locations.'
     'OI': 'Online', 'You’ll complete classes and assignments online, allowing you to interact with faculty, have discussions, and encourage your classmates all from a distance.'
     'F2': 'Face to face #2', 'You’ll meet for classes at one of our convenient locations and move through your program with the same cohort of supportive colleagues.' 
     'O': 'Online', 'You’ll complete classes and assignments online, allowing you to interact with faculty, have discussions, and encourage your classmates all from a distance.'
