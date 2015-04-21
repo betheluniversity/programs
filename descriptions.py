56c47692a209413dd9167a72de43ddfe9de03a5e
Eric Jameson <e-jameson@bethel.edu>
2015-04-21 09:47:17 -0500

    safe characters

--- descriptions.py
+++ descriptions.py
@@ -2,9 +2,9 @@ __author__ = 'ejc84332'
 
 
 delivery_descriptions = {
-    'HL': "Courses will be a mix of online and face to face sessions. You’ll complete 30-80% of your coursework online.",
-    'H': "Courses will be a mix of online and face to face sessions. You’ll complete 30-80% of your coursework online.",
-    'HH': "Courses will be a mix of online and face to face sessions. You’ll complete 30-80% of your coursework online.",
+    'HL': "Courses will be a mix of online and face to face sessions. You'll complete 30-80% of your coursework online.",
+    'H': "Courses will be a mix of online and face to face sessions. You'll complete 30-80% of your coursework online.",
+    'HH': "Courses will be a mix of online and face to face sessions. You'll complete 30-80% of your coursework online.",
     'OI': "You’ll complete coursework online, with 1-2 week on-campus intensives each year.",
     'F2': "You'll meet for classes at one of our convenient locations.",
     'O': "You’ll complete 80-100% of your coursework online.",
