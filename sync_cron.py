c8058e2fb40e89ab7326fb8d6abc07abca13608f
c-schwarze <ces55739@bethel.edu>
2019-03-19 16:31:14 -0500

    go back to no yield solution

--- sync_cron.py
+++
@@ -1,9 +0,0 @@
-from sync import CascadeBlockProcessor
-
-cbp = CascadeBlockProcessor()
-
-time_interval = 1
-send_email = True
-yield_output = False
-
-cbp.process_all_blocks(time_interval, send_email, yield_output)
